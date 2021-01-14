5633f3fdf1e693dfbd38f40447f2266a4b40a46b
John Cris <36905188+johncris09@users.noreply.github.com>
2021-01-14 16:43:44 +0800

    index function

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,6 +6,11 @@ class Dashboard extends CI_Controller {
     public function __construct()
     {
         parent:: __construct();
-    } 
+    }
     
+	public function index()
+	{
+		$data['page_title'] = "Dashboard";
+		$this->load->view('admin/dashboard', $data);
+    }
 }
