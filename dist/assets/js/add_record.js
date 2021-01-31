fbb7a967f9a1c49528abc6cbc8443d1dc7cb2a05
John Cris <36905188+johncris09@users.noreply.github.com>
2021-01-31 16:25:37 +0800

    init

---
+++ dist/assets/js/add_record.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Class definition
+var AddRecord = function () {
+    var _init = function () {   
+        
+    };
+
+    return {
+        // Init
+        init: function () {
+            _init();
+        },
+    };
+}(); 
+
+// Class Initialization
+jQuery(document).ready(function () {
+    AddRecord.init();
+});
