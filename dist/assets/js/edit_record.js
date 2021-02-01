02001971ef34160250a89552afbe872b69be41db
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-01 12:59:36 +0800

    init

---
+++ dist/assets/js/edit_record.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Class definition
+var EditRecord = function () {
+    var _init = function () {   
+    };
+
+    return {
+        // Init
+        init: function () {
+            _init();
+        },
+    };
+}(); 
+
+// Class Initialization
+jQuery(document).ready(function () {
+    EditRecord.init();
+});
