c57fc4565cbf4b8199b5f7c991f81f604c460d24
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-03 09:11:39 +0800

    init

---
+++ application/controllers/User.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller { 
+
+    public function __construct()
+    {
+        parent:: __construct();
+
+        if (!$this->session->userdata('user_id')) {
+            redirect('login');
+        }
+    }
+    
+}
