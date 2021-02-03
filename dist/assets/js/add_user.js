c756f967fdcb61889517ce7ed776925ce636eb6c
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-03 09:48:36 +0800

    init

---
+++ dist/assets/js/add_user.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Class definition
+var AddUser = function () {
+    var _init = function () {  
+
+    };
+
+    return {
+        // Init
+        init: function () {
+            _init();
+        },
+    };
+}(); 
+
+// Class Initialization
+jQuery(document).ready(function () {
+    AddUser.init();
+});
