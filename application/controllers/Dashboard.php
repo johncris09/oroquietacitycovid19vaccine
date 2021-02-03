c3d14b675f60d2469093e3750b616ebe5cef0097
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-03 14:04:22 +0800

    signout function

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -19,5 +19,18 @@ class Dashboard extends CI_Controller {
 		$this->load->view('admin/dashboard', $data);
     }
 
+    public function signout()
+    { 
+        
+        $all_sessions = $this->session->all_userdata();
+
+        // unset all sessions
+        foreach ($all_sessions as $key => $val) {
+            $this->session->unset_userdata($key);
+        }
+
+         redirect('login');
+    }
+
 
 }
