d7c81a91e733b1118c8f6d9ca7c8c8dbd40cbadd
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-03 13:50:55 +0800

    init

---
+++ dist/assets/js/dashboard.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Class definition
+var RecordChart = function () {
+    var _init = function () {   
+ 
+    };
+
+    return {
+        // Init
+        init: function () {
+            _init();
+        },
+    };
+}();
+
+
+
+// Class Initialization
+jQuery(document).ready(function () {
+    RecordChart.init();
+});
