621146f8cb1790551851327ede124e0a52cb23a9
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-03 09:14:56 +0800

    init

---
+++ dist/assets/js/user.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Class definition
+var User = function () {
+    var _init = function () {      
+
+ 
+    };
+
+    return {
+        // Init
+        init: function () {
+            _init();
+        },
+    };
+}();
+
+
+
+// Class Initialization
+jQuery(document).ready(function () {
+    User.init();
+});
