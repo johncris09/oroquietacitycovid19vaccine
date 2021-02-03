94daee03529df59c2176e628fadef1c1c580298c
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-03 14:03:43 +0800

    dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,11 +6,11 @@ class Dashboard extends CI_Controller {
     public function __construct()
     {
         parent:: __construct();
+
+        if (!$this->session->userdata('user_id')) {
+            redirect('login');
+        }
     }
-    
-	public function index()
-	{
-		$data['page_title'] = "Dashboard";
-		$this->load->view('admin/dashboard', $data);
-    }
+
+
 }
