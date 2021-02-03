aace98482f095ddae56ea44511baaaf982752a9c
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-03 09:37:45 +0800

    get user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -18,4 +18,23 @@ class User extends CI_Controller {
 		$this->load->view('admin/user', $data);
     }
     
+
+	public function get_user()
+	{ 
+		$data = [];
+		$user = $this->user_model->user();
+		foreach($user as $row){
+			unset($row['password']);
+			$row['dateregistered'] = date('M d, Y h:i:s a', strtotime($row['dateregistered']));
+			$row['firstname'] = ucwords($row['first_name']);
+			$row['lastname'] = ucwords($row['last_name']);
+			$row['middlename'] = ucwords($row['middle_name']);
+			$row['extension'] = ucwords($row['extension']);
+			$row['username'] = ucwords($row['username']);
+			$row['Actions']  = null;
+			$data['data'][] = $row; 
+
+		} 
+		echo json_encode($data);
+	} 
 }
