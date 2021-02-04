90ed32b7123c67a74d01b747600e557a1f282ea9
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-04 19:57:19 +0800

    working in record chart

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,8 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-
+	<head>
 		<?php $this->view('template/meta-info.php'); ?>
 		<?php $this->view('template/css-link.php'); ?>
 
@@ -15,21 +13,131 @@
 				<div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
 					<?php $this->view('template/header.php'); ?>
 					<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
-						<?php $this->view('template/sub-header.php'); ?>
-						<div class="d-flex flex-column-fluid">
-							<div class="container">
+						<div class="subheader py-2 py-lg-6 subheader-solid" id="kt_subheader">
+							<div class="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+								<div class="d-flex align-items-center flex-wrap mr-1">
+									<div class="d-flex align-items-baseline flex-wrap mr-5">
+										<h5 class="text-dark font-weight-bold my-1 mr-5"><?php echo $page_title; ?></h5>
+									</div>
+								</div>
+								<div class="d-flex align-items-center">
+									<a href="<?php echo base_url(); ?>record/add" title="New Record" class="btn btn-outline-primary btn-sm font-weight-bold font-size-base mr-1"  >
+										<i class="fas fa-plus-circle"></i>	New Record
+									</a> 
+								</div>
+							</div> 
+							</div>
+						</div>
+						
+					<div class="d-flex flex-column-fluid">
+						<div class="container">
+							
+							
+							<div class="row">
+								<div class="col">
+									<div class="card card-custom gutter-b">
+										<div class="card-header">
+											<div class="card-title">
+												<h3 class="card-label">
+													Record Chart
+												</h3>
+											</div>
+
+											<div class="card-toolbar">
+												<div class="dropdown dropdown-inline mr-2">
+													<button type="button"
+														class="btn btn-primary font-weight-bolder dropdown-toggle"
+														data-toggle="dropdown" aria-haspopup="true"
+														aria-expanded="false">Filter by</button>
+													<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
+														<ul class="navi flex-column navi-hover py-2">
+															<li
+																class="navi-header font-weight-bolder text-uppercase font-size-sm text-primary pb-2">
+																Choose an option:</li>
+															<li class="navi-item">
+																<a href="javascript:;" class="navi-link filter-by" data-fileter-by="week">
+																	<span class="navi-text"> <i class="far fa-circle"></i> Week</span>
+																</a>
+															</li>
+															<li class="navi-item">
+																<a href="javascript:;" class="navi-link filter-by" data-fileter-by="month">
+																	<span class="navi-text"><i class="far fa-circle"></i> Month</span>
+																</a>
+															</li>
+															<li class="navi-item">
+																<a href="javascript:;" class="navi-link filter-by" data-fileter-by="year">
+																	<span class="navi-text"><i class="far fa-circle"></i> Year</span>
+																</a>
+															</li>
+														</ul>
+													</div>
+												</div>
+											</div> 
+										</div>
+
+										<div class="card-body">
+											<form id="record-date-range-form" method="post" class="mb-15">
+												<div class="row justify-content-center">
+													<div class="col-6">
+														<label><strong>Date Range</strong></label>
+														<div class="input-daterange input-group" id="date-range">
+															<input type="text" class="form-control datatable-input"
+																name="start" autocomplete="off" placeholder="From" required="" />
+															<div class="input-group-append">
+																<span class="input-group-text">
+																	<i class="la la-ellipsis-h"></i>
+																</span>
+															</div>
+															<input type="text" class="form-control datatable-input"
+																name="end" autocomplete="off"  placeholder="To"  required="" />
+															<div class="input-group-append">
+																<button type="submit" class="btn btn-primary btn-primary--icon">
+																	<span>
+																		<i class="fa fa-check"></i>
+																		<span>Ok</span>
+																	</span>
+																</button>
+															</div>
+															<div class="input-group-append">
+																<button type="button" id="reset" class="btn btn-secondary btn-secondary--icon">
+																	<span>
+																		<i class="la la-close"></i>
+																		<span>Reset</span>
+																	</span>
+																</button>
+															</div>
+														</div>
+													</div>
+												</div>
+											</form>
+											<div id="record-chart"></div>
+										</div>
+									</div>
+								</div>
+
 							</div>
 						</div>
 					</div>
-					<?php $this->view('template/footer.php'); ?>
+					</div>
 				</div>
 			</div>
 		</div>
+	<?php $this->view('template/footer.php'); ?>
+	
+	<?php $this->view('template/quick-panel.php'); ?>
+	<?php $this->view('template/js-src.php'); ?>
 
-		<?php $this->view('template/quick-panel.php'); ?>
-
-
-		<?php $this->view('template/js-src.php'); ?>
+	<script src="<?php echo base_url(); ?>dist/assets/js/dashboard.js"></script> 
+	<script type="text/javascript">
+		$('#date-range').datepicker({
+			todayHighlight: true,
+			templates: {
+				leftArrow: '<i class="la la-angle-left"></i>',
+				rightArrow: '<i class="la la-angle-right"></i>',
+			},
+			dateFormat: 'yyyy/m/dd',
+		});
+	</script>
 
 	</body>
 </html>
