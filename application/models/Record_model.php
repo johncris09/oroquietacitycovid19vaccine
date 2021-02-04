0574ec1801765c01083436a1498127990ab95324
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-04 19:54:20 +0800

    working in record chart

--- application/models/Record_model.php
+++ application/models/Record_model.php
@@ -15,6 +15,7 @@ class Record_model extends CI_Model
          // $this->db->cache_on();
         return $this->db
             ->where('deletestatus', 0)
+            ->order_by('timestamp','DESC')
         	->get('record')
         	->result_array();
     }
@@ -64,4 +65,90 @@ class Record_model extends CI_Model
     }
 
 
+
+    public function registered_today()
+    {
+        return $this->db
+            ->where('deletestatus', 0)
+            ->where('year(timestamp)', date('Y'))
+            ->where('month(timestamp)', date('m'))
+            ->where('day(timestamp)', date('d'))
+            ->get('record')
+            ->num_rows();
+    }
+
+    public function record_chart()
+    {
+        return $this->db
+            ->select('count(*) tot, timestamp')
+            ->where('deletestatus', 0)
+            ->where('timestamp <= CURRENT_DATE() ')
+            ->group_by('day(timestamp)')
+            ->order_by('timestamp asc')
+            ->get('record')
+            ->result_array();
+
+
+    }
+    
+    public function record_this_week($date)
+    {
+        return $this->db
+            ->select('count(*) tot, timestamp')
+            ->where('date(timestamp) =', $date)
+            // ->where('date(timestamp) <=', $end_date)
+            ->group_by('day(timestamp)')
+            ->order_by('timestamp', 'asc')   
+            ->get('record')
+            ->result_array();
+    }
+
+     public function record_this_month($date)
+    {
+        return $this->db
+            ->select('count(*) tot, timestamp')
+            ->where('date(timestamp) =', $date)
+            // ->where('date(timestamp) <=', $end_date)
+            ->group_by('day(timestamp)')
+            ->order_by('timestamp', 'asc')   
+            ->get('record')
+            ->result_array();
+    }
+
+
+    public function record_by_month($date)
+    {
+        return $this->db
+            ->select('count(*) tot, timestamp')
+            ->where('year(timestamp) =', date('Y'))
+            ->where('month(timestamp) =', date('m', strtotime($date)))
+            ->group_by('month(timestamp)')
+            ->order_by('timestamp', 'asc')   
+            ->get('record')
+            ->result_array();
+    }
+
+
+    public function record_by_year($end_year)
+    {
+        return $this->db
+            ->select('count(*) tot, timestamp')
+            ->where('year(timestamp) <=', $end_year)
+            ->group_by('year(timestamp)')
+            ->order_by('timestamp', 'asc')   
+            ->get('record')
+            ->result_array();
+    }
+
+    public function record_by_date_range($start_date, $end_date)
+    {
+        return $this->db
+            ->select('count(*) tot, timestamp')
+            ->where('date(timestamp) >=', $start_date)
+            ->where('date(timestamp) <=', $end_date)
+            ->group_by('day(timestamp)')
+            ->order_by('timestamp', 'asc')   
+            ->get('record')
+            ->result_array();
+    }
 }
