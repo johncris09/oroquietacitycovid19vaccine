f5c8a39970549c12a77e5e58626a92719f41268b
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-26 10:40:20 +0800

    desktop header

---
+++ application/views/template/header.php
@@ -0,0 +1,17 @@
+
+					<div id="kt_header" class="header header-fixed">
+						<div class="container-fluid d-flex align-items-stretch justify-content-between">
+							<div class="header-menu-wrapper header-menu-wrapper-left" id="kt_header_menu_wrapper"></div>
+							<div class="topbar">
+								<div class="topbar-item">
+									<div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
+										<span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
+										<span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3"><?php echo ucwords($_SESSION['username']); ?></span>
+										<span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
+											<span class="symbol-label font-size-h5 font-weight-bold"><?php echo ucwords($_SESSION['username'][0]); ?></span>
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
