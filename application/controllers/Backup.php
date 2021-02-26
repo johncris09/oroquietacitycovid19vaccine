57501146a3aaa0c7d7a0ac7414f0c51a23d832a4
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-26 10:25:29 +0800

    init

---
+++ application/controllers/Backup.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Backup extends CI_Controller { 
+
+    public function __construct()
+    {
+        parent:: __construct(); 
+
+        if (!$this->session->userdata('user_id')) {
+            redirect('login');
+        }
+	}
+ 
+    
+}
