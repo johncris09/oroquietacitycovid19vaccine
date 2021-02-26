f65169f04b3acf6a916bc9d0d22f703055b894fa
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-26 10:27:07 +0800

    login function

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,6 +6,10 @@ class Login extends CI_Controller {
     public function __construct()
     {
         parent:: __construct();
+
+        if ( $this->session->userdata('user_id') ) {
+            redirect('dashboard');
+        }
     }
     
 	public function index()
@@ -14,5 +18,28 @@ class Login extends CI_Controller {
 		$this->load->view('login', $data);
     }
     
+	public function login()
+	{
+		$user_info = array(
+            'username' => $this->input->post('username'),
+            'password' => md5($this->input->post('password')),
+        );
+        
+        $login = $this->user_model->login($user_info); 
+
+        if ($login > 0) {     
+
+            // set session 
+            $this->set_session($user_info); 
+
+            $data["response"] = true;
+
+        } else {
+            $data["response"] = false;
+            $data["message"] = 'Invalid Username/Password. Please try again.';
+        }
+
+        echo json_encode($data);
+    }
     
 }
