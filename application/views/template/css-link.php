c3e83df48e5fc1bb8086905cf35304cc4dcb71e6
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-26 10:39:05 +0800

    css links

---
+++ application/views/template/css-link.php
@@ -0,0 +1,13 @@
+
+
+		
+		<link href="<?php echo base_url(); ?>dist/assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
+
+		<link href="<?php echo base_url(); ?>dist/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+		<link href="<?php echo base_url(); ?>dist/assets/plugins/custom/prismjs/prismjs.bundle.css" rel="stylesheet" type="text/css" />
+		<link href="<?php echo base_url(); ?>dist/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+
+		<link href="<?php echo base_url(); ?>dist/assets/css/themes/layout/header/base/light.css" rel="stylesheet" type="text/css" />
+		<link href="<?php echo base_url(); ?>dist/assets/css/themes/layout/header/menu/light.css" rel="stylesheet" type="text/css" />
+		<link href="<?php echo base_url(); ?>dist/assets/css/themes/layout/brand/dark.css" rel="stylesheet" type="text/css" />
+		<link href="<?php echo base_url(); ?>dist/assets/css/themes/layout/aside/dark.css" rel="stylesheet" type="text/css" />
