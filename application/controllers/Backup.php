5cba2e010e5a3e9374dee4de235959868da8b75a
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-26 10:26:07 +0800

    export function

--- application/controllers/Backup.php
+++ application/controllers/Backup.php
@@ -19,6 +19,42 @@ class Backup extends CI_Controller {
 		$this->load->view('admin/backup', $data);
 	}
 
+
+    public function export()
+    {
+        // Load the DB utility class
+        $this->load->dbutil();
+
+        // get all the table
+        $tables = $this->db->list_tables();
+
+        // data preferences
+        $prefs = array(
+            'tables'             => $tables,   // Array of tables to backup.
+            'ignore'             => array(),   // List of tables to omit from the backup
+            'format'             => 'sql',     // gzip, zip, txt
+            'add_drop'           => true,      // Whether to add DROP TABLE statements to backup file
+            'add_insert'         => true,      // Whether to add INSERT data to backup file
+            'newline'            => "\n",      // Newline character used in backup file
+            'foreign_key_checks' => false,
+        );
+
+        // Backup your entire database and assign it to a variable
+        $backup   = $this->dbutil->backup($prefs);
+        $filename = date("Y-m-d h.i.s") . ' ' . $this->db->database . '.sql';
+
+        if (write_file(FCPATH . '/database/' . $filename, $backup)) {
+            $data = array(
+                'response' => true,
+                'message' => 'Database Exported.');
+        } else {
+            $data = array(
+                'response' => false,
+                'message'  => $this->db->error()['message']);
+        }  
+
+        echo json_encode($data);
+    }
  
     
 }
