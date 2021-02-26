fd2bea693cd6ca8a57984a1fcacf47dc85c7bdf8
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-26 10:41:47 +0800

    sub header

---
+++ application/views/template/sub-header.php
@@ -0,0 +1,10 @@
+
+						<div class="subheader py-2 py-lg-6 subheader-solid" id="kt_subheader">
+							<div class="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+								<div class="d-flex align-items-center flex-wrap mr-1">
+									<div class="d-flex align-items-baseline flex-wrap mr-5">
+										<h5 class="text-dark font-weight-bold my-1 mr-5"><?php echo $page_title; ?></h5>
+									</div>
+								</div>
+							</div>
+						</div>
