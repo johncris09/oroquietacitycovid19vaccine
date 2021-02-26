8e91a071fcfbde7ca4ca412cf8d31c20d3e55999
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-26 10:39:47 +0800

    footer

---
+++ application/views/template/footer.php
@@ -0,0 +1,11 @@
+					
+					<div class="footer bg-white py-4 d-flex flex-lg-column" id="kt_footer">
+						<div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
+							<div class="text-dark order-2 order-md-1">
+								<span class="text-muted font-weight-bold mr-2">&copy;<?php echo date('Y'); ?></span>
+								<a href="#" target="_blank" class="text-dark-75 text-hover-primary">LGU Oroquieta City - MIS Department</a>
+							</div>
+							<div class="nav nav-dark">
+							</div>
+						</div>
+					</div>
