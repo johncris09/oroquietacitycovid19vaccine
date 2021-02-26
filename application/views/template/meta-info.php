d8339622c121b12e9dbb516dd6e0b63a10ef22e1
John Cris <36905188+johncris09@users.noreply.github.com>
2021-02-26 10:41:09 +0800

    metadata

---
+++ application/views/template/meta-info.php
@@ -0,0 +1,7 @@
+
+
+
+		<meta charset="utf-8" />
+		<title><?php echo $page_title; ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<link rel="shortcut icon" href="<?php echo base_url(); ?>dist/assets/media/logos/favicon.ico" />
