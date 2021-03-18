1af2dda6888af8b214202123c02ed16ac8762136
John Cris <36905188+johncris09@users.noreply.github.com>
2021-03-18 15:09:17 +0800

    get encoder log

--- application/controllers/Encoder_log.php
+++ application/controllers/Encoder_log.php
@@ -17,4 +17,22 @@ class Encoder_log extends CI_Controller {
 		$data['page_title'] = "Encoder's Log";
 		$this->load->view('admin/encoder_log', $data);
     } 
+	
+	public function get_encoder_log()
+	{ 
+		$log = $this->log_model->encoder_log();
+		if( $log->num_rows() ){
+			foreach( $log->result_array() as $row ){
+				unset($row['password']);
+				$row['time'] = date('h:i:s a', strtotime($row['date'])); 
+				$row['date'] = date('m/d/Y', strtotime($row['date'])); 
+				$row['user'] = ucwords( $row['first_name'] . ' ' . $row['middle_name'] . ' ' . $row['last_name'] );
+				$row['Actions']  = null;
+				$data['data'][] = $row;
+			}
+		}else{
+			$data['data'] = array();
+		}
+		echo json_encode($data);
+	}
 }
