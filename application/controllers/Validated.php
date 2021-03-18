ce2b2d722fb3e45a30c55e05e9bc246ea6dc52c7
John Cris <36905188+johncris09@users.noreply.github.com>
2021-03-18 15:28:48 +0800

    update validate function

--- application/controllers/Validated.php
+++ application/controllers/Validated.php
@@ -160,5 +160,68 @@ class Validated extends CI_Controller {
     	$data['page_title'] = "Edit Record";
     	$data['validated'] = $this->record_model->get_record($id);
 		$this->load->view('admin/edit_validated', $data);
+	}
+
+	public function update($id)
+	{
+		$data = array(
+			'id' => $id,
+			'lastname' => trim($this->input->post('lastname')),
+			'firstname' => trim($this->input->post('firstname')),
+			'middlename' => trim($this->input->post('middlename')),
+			'gender' => $this->input->post('gender'), 
+			'contactnumber' => trim($this->input->post('contactnumber')),
+			'purok' => $this->input->post('purok'), 
+			'street' => trim($this->input->post('street')),
+			'barangay' => trim($this->input->post('barangay')),
+			'birthdate' => date('Y-m-d', strtotime($this->input->post('birthdate'))),
+			'registeredvoter' => $this->input->post('registeredvoter'), 
+			'governmentissuedid' => $this->input->post('governmentissuedid'), 
+			'idnumber' => $this->input->post('idnumber'), 
+			'position' => $this->input->post('position'),
+			'occupation' => $this->input->post('occupation'), 
+			'covidpositive' => $this->input->post('CovidPositive'), 
+			'covidpositivecontact' => $this->input->post('CovidPositiveContact'), 
+			'travelled' => $this->input->post('Travelled'), 
+			'mingled' => $this->input->post('Mingled'), 
+			'optionIllness_1' => !empty($_POST['OptionIllness_1']) ?  implode (", ", $this->input->post('OptionIllness_1'))  : '' ,
+			'dogbite' => $this->input->post('DogBite'), 
+			'vaccinelast4weeks' => $this->input->post('VaccineLast4Weeks'), 
+			'bloodtransfusion' => $this->input->post('BloodTransfusion'), 
+			'takedrugs' => $this->input->post('TakeDrugs'), 
+			'allergy' => $this->input->post('Allergy'), 
+			'vaccinereaction' => $this->input->post('VaccineReaction'), 
+			'optionIllness_2' => !empty($_POST['OptionIllness_2']) ?  implode (", ", $this->input->post('OptionIllness_2'))  : '' ,
+			'other_illness' => $this->input->post('other_illness'),
+			'pregnant' => $this->input->post('Pregnant'), 
+			'breastfeed' => $this->input->post('Breastfeed'), 
+			'ClinicalStudy' => $this->input->post('ClinicalStudy'),
+		);
+		
+
+		$update = $this->record_model->update($data);
+		if($update > 0){
+
+            $log_data = array(
+                'description' => 'updated a record whose name is "' . $data['firstname']  . $data['middlename'] . $data['lastname'] . '"',
+                'user_id' => $_SESSION['user_id'],
+                'date' => date('Y-m-d H:i:s'),
+            );
+
+            $this->log_model->insert( $log_data );
+
+			$data = array(
+				'response' => true,
+				'message'  => 'Data updated successfully!',
+			);
+  
+		}else{ 
+			$data = array(
+				'response' => false,
+				'message'  => 'Data not updated!',
+				// 'message' => $this->db->error()['message'],
+			);
+		} 
+		echo json_encode($data); 
 	} 
 }
