8e85023ad2a2cb16572af9aad4b3928d64559291
John Cris <36905188+johncris09@users.noreply.github.com>
2021-03-18 15:22:25 +0800

    get all validated

---
+++ application/models/Validated_model.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Validated_model extends CI_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function record()
+    {
+        return $this->db
+            ->where('deletestatus', 0)
+            ->where('validated', 'no')
+            ->order_by('timestamp','DESC')
+        	->get('record')
+        	->result_array();
+    }
+
+
+
+    public function validated()
+    {
+        return $this->db
+            ->where('validated.record_id = record.id')
+            ->where('record.validated', 'yes')
+            ->order_by('validated.timestamp','DESC')
+            ->get('record, validated');
+            // ->result_array();
+    }
+}
