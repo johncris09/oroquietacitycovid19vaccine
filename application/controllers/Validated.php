4d9e3cd64a1bffba198476871452cff10ec5636d
John Cris <36905188+johncris09@users.noreply.github.com>
2021-03-18 15:27:14 +0800

    get all validated

--- application/controllers/Validated.php
+++ application/controllers/Validated.php
@@ -22,5 +22,33 @@ class Validated extends CI_Controller {
     {
     	$data['page_title'] = "Add Record";
 		$this->load->view('admin/add_validated', $data);
-    } 
+    }
+
+	
+	public function get_validated()
+	{
+		$validated = $this->validated_model->validated();
+		if( $validated->num_rows() ){
+			foreach( $validated->result_array() as $row ){
+				$row['dateregistered'] = date('m/d/Y', strtotime($row['timestamp']));
+				$row['time'] = date('h:i:s a', strtotime($row['timestamp']));
+				$birthDate = date('m/d/Y', strtotime($row['birthdate']));
+				$birthDate = explode("/", $birthDate);
+				$age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+				    ? ((date("Y") - $birthDate[2]) - 1)
+				    : (date("Y") - $birthDate[2]));
+				$row['age'] = $age;
+				$row['lastname'] = ucwords($row['lastname']);
+				$row['firstname'] = ucwords($row['firstname']);
+				$row['middlename'] = ucwords($row['middlename']);
+				$row['street'] = ucwords($row['street']);
+				$row['Actions']  = null;
+				$data['data'][] = $row; 
+
+			}
+		}else{
+			$data['data'] = array();
+		} 
+		echo json_encode($data);
+	}  
 }
