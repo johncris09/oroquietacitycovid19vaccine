7837c4fb8170a6f7e8a04f73efe19cc9faa86445
John Cris <36905188+johncris09@users.noreply.github.com>
2021-03-19 15:24:32 +0800

    barangay chart

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -57,7 +57,7 @@ class Dashboard extends CI_Controller {
 
     public function record_chart()
     {
-        // $_POST['filter_by'] = 'week';
+        // $_POST['filter_by'] = 'this month';
         $data = [];
         if($_POST['filter_by'] == 'this month'){
             $start_date = date('Y-m-d', strtotime('first day of this month'));
@@ -219,6 +219,208 @@ class Dashboard extends CI_Controller {
         }
 
 
+        echo json_encode($data);
+    }
+
+
+    public function barangay_chart()
+    {
+        // $_POST['filter_by'] = 'this month';
+        // $data = [];
+        // if($_POST['filter_by'] == 'this month'){
+        //     // $start_date = date('Y-m-d', strtotime('first day of this month'));
+        //     // $end_date   = date('Y-m-d', strtotime('today'));
+        //     // $today = (int)date('d', strtotime('today'));
+
+        //     foreach ($this->config->item('barangay') as $barangay) {
+        //         $pre_registered = $this->record_model->barangay_record_for_pre_registered($barangay);
+        //         if(!empty($pre_registered)){
+        //             $data['pre_registered'][]       = (int)$pre_registered[0]['tot'];
+        //             $data['categories'][]           = $barangay;
+        //         }else{
+        //             $data['pre_registered'][]       = (int)0;
+        //             $data['categories'][]           = $barangay;
+        //         }
+
+        //         $pre_registered = $this->record_model->barangay_record_for_pre_registered($barangay);
+        //         if(!empty($pre_registered)){
+        //             $data['pre_registered'][]       = (int)$pre_registered[0]['tot'];
+        //             $data['categories'][]           = $barangay;
+        //         }else{
+        //             $data['pre_registered'][]       = (int)0;
+        //             $data['categories'][]           = $barangay;
+        //         }
+        //     }
+
+        //     // while (strtotime($start_date) <= strtotime($end_date)) {
+        //     //     $date = $start_date;
+        //     //     $record = $this->record_model->barangay_record_this_month($date, $barangay); 
+        //     //     if(!empty($record)){
+        //     //         $data['pre_registered'][]       = $record[0]['tot'];
+        //     //         $data['categories'][]           = date('d', strtotime($date));
+        //     //     }else{
+        //     //         $data['pre_registered'][]       = 0;
+        //     //         $data['categories'][]           = date('d', strtotime($date));
+        //     //     }
+
+
+        //     //     // $validated = $this->validated_model->barangay_validated_this_month($date); 
+        //     //     // if(!empty($validated)){
+        //     //     //     $data['validated'][]        = $validated[0]['tot'];
+        //     //     // }else{
+        //     //     //     $data['validated'][]        = 0;
+        //     //     // }
+        //     //     $start_date = date ("Y-m-d", strtotime("+1 days", strtotime($date)));
+        //     // }
+
+
+
+        //     // $start_date = date('Y-m-d', strtotime('first day of this month'));
+        //     // $from           = date('M. d, Y', strtotime($start_date));
+        //     // $to             = date('M. d, Y', strtotime($end_date));
+        //     // $data['range']  = $from . ' - ' . $to;
+
+        // }else if($_POST['filter_by'] == 'week'){
+
+        //     $start_date = date('Y-m-d',  strtotime('previous ' . date('l',  strtotime('today')) . ' +1 day'));
+        //     $end_date   = date('Y-m-d',  strtotime('today'));
+        //     $begin      = new DateTime($start_date);
+        //     $end        = new DateTime($end_date);
+        //     $interval   = DateInterval::createFromDateString('1 day');
+        //     $period     = new DatePeriod($begin, $interval, $end);
+        //     foreach ($period as $dt) {
+        //         $date   = $dt->format("Y-m-d");
+        //         $record = $this->record_model->record_this_week($date);
+        //         if(!empty($record)){
+        //             $data['pre_registered'][]         = $record[0]['tot'];
+        //             $data['categories'][]   = date('D', strtotime($date));
+        //         }else{
+        //             $data['pre_registered'][]         = 0;
+        //             $data['categories'][]   = date('D', strtotime($date));
+        //         }
+
+
+        //         $validated = $this->validated_model->validated_this_week($date);
+        //         if(!empty($validated)){
+        //             $data['validated'][]         = $validated[0]['tot'];
+        //         }else{
+        //             $data['validated'][]         = 0;
+        //         }
+                
+        //     }
+        //     $from           = date('M. d, Y', strtotime($start_date));
+        //     $to             = date('M. d, Y', strtotime($end_date));
+        //     $data['range']  = $from . ' - ' . $to;
+
+        // }else if($_POST['filter_by'] == 'month'){ 
+        //     $start_date = date('M. Y', strtotime(date('Y-01')));
+        //     $end_date   = date('M. Y', strtotime(date('Y-12')));
+        //     for($i = 1 ; $i <= 12; $i++){
+        //         $date = date('Y-m-d', strtotime( date('Y') . '/' . $i . '/01'));
+        //         if( date('m', strtotime('this month')) == $i ){
+        //             $record = $this->record_model->record_by_month($date);
+        //             if(!empty($record)){
+        //                 $data['pre_registered'][]       = $record[0]['tot'];
+        //                 $data['categories'][]           = date('M', strtotime($date));
+        //             }else{
+        //                 $data['pre_registered'][]       = 0;
+        //                 $data['categories'][]           = date('M', strtotime($date));
+        //             }
+
+
+        //             $validated = $this->validated_model->validated_by_month($date);
+        //             if(!empty($validated)){
+        //                 $data['validated'][]       = $validated[0]['tot'];
+        //             }else{
+        //                 $data['validated'][]       = 0;
+        //             }
+
+        //             break;
+
+        //         }else{
+        //             $record = $this->record_model->record_by_month($date);
+        //             if(!empty($record)){
+        //                 $data['pre_registered'][]         = $record[0]['tot'];
+        //                 $data['categories'][]   = date('M', strtotime($date));
+        //             }else{
+        //                 $data['pre_registered'][]         = 0;
+        //                 $data['categories'][]   = date('M', strtotime($date));
+        //             }
+
+
+        //             $validated = $this->validated_model->validated_by_month($date);
+        //             if(!empty($validated)){
+        //                 $data['validated'][]       = $validated[0]['tot']; 
+        //             }else{
+        //                 $data['validated'][]       = 0;
+        //             }
+
+        //             continue;
+        //         }
+                
+        //     }
+
+
+        //     $from    = date('M. Y', strtotime($start_date));
+        //     $to      = date('M. Y', strtotime($end_date));
+        //     $data['range'] = $from . ' - ' . $to;
+
+        // }else if($_POST['filter_by'] == 'year'){
+        //     $end_year = date('Y');
+
+        //     $record = $this->record_model->record_by_year($end_year);
+        //     foreach($record as $row){
+        //         $data['pre_registered'][]     = $row['tot'];
+        //         $data['categories'][] = date('Y', strtotime($row['date_registered']));
+        //     }
+
+        //     $validated = $this->validated_model->validated_by_year($end_year);
+        //     foreach($validated as $row){
+        //         $data['validated'][]     = $row['tot'];
+        //     }
+
+        //     $to = date('Y', strtotime($end_year));
+        //     $data['range'] = $data['categories'][0] . ' - ' . $to;
+
+        // }else{
+
+        //     $start_date = date('Y-m-d', strtotime(date($_POST['filter_by'][0])));
+        //     $end_date   = date('Y-m-d', strtotime(date($_POST['filter_by'][1])));
+
+        //     $record = $this->record_model->record_by_date_range($start_date, $end_date);
+        //     $data['record'] = $record;
+        //     foreach($record as $row){
+        //         $data['data'][]     = $row['tot'];
+        //         $data['categories'][] = date('m/d/y', strtotime($row['date_registered']));
+        //     }
+
+        //     $from    = date('M. d, Y', strtotime($start_date));
+        //     $to      = date('M. d, Y', strtotime($end_date));
+        //     $data['range'] = $from . ' - ' . $to;
+
+        // }
+        // 
+        $sum = 0;
+        foreach ($this->config->item('barangay') as $barangay) {
+            $pre_registered = $this->record_model->barangay_record_for_pre_registered($barangay);
+            if(!empty($pre_registered)){
+                $data['pre_registered'][]       = (int)$pre_registered[0]['tot'];
+                $data['categories'][]           = $barangay;
+            }else{
+                $data['pre_registered'][]       = 0;
+                $data['categories'][]           = $barangay;
+            }
+
+            $validated = $this->validated_model->barangay_record_for_validated($barangay);
+            if(!empty($validated)){
+                $sum+= (int)$validated[0]['tot'];
+                $data['validated'][]       = (int)$validated[0]['tot'];
+            }else{
+                $data['validated'][]       = 0; 
+            }
+        }
+
+        $data['sum'] = $sum;
         echo json_encode($data);
     }
 
