7f951bd342a0f4839cd72744cc44a05781d82234
John Cris <36905188+johncris09@users.noreply.github.com>
2021-04-12 14:21:50 +0800

    update css source

--- application/views/template/css-link.php
+++ application/views/template/css-link.php
@@ -1,7 +1,6 @@
 
 
 		
-		<link href="<?php echo base_url(); ?>dist/assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
 
 		<link href="<?php echo base_url(); ?>dist/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
 		<link href="<?php echo base_url(); ?>dist/assets/plugins/custom/prismjs/prismjs.bundle.css" rel="stylesheet" type="text/css" />
@@ -12,4 +11,16 @@
 		<link href="<?php echo base_url(); ?>dist/assets/css/themes/layout/brand/dark.css" rel="stylesheet" type="text/css" />
 		<link href="<?php echo base_url(); ?>dist/assets/css/themes/layout/aside/dark.css" rel="stylesheet" type="text/css" />
 
-		<link href="<?php echo base_url(); ?>dist/assets/css/pages/typeahead.css" rel="stylesheet" type="text/css" />
+		<!-- <link href="<?php echo base_url(); ?>dist/assets/css/pages/typeahead.css" rel="stylesheet" type="text/css" /> -->
+
+		<!-- Local DataTable Stylesheet -->
+		<link href="<?php echo base_url(); ?>dist/assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
+		
+		<!-- <link href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
+		<link href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />
+		<link href="https://cdn.datatables.net/scroller/2.0.3/css/scroller.dataTables.min.css" rel="stylesheet" type="text/css" /> -->
+
+
+		<!-- DataTable Stylesheet -->
+		<!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.24/b-1.7.0/b-colvis-1.7.0/b-html5-1.7.0/b-print-1.7.0/sl-1.3.3/datatables.min.css"/> -->
+ 
