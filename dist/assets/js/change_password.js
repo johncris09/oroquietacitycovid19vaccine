ea3fa3d1cc97a06a986d585ef21b52a57f2cef2e
John Cris <36905188+johncris09@users.noreply.github.com>
2021-04-12 14:11:30 +0800

    add password confirmation

--- dist/assets/js/change_password.js
+++ dist/assets/js/change_password.js
@@ -20,7 +20,11 @@ var ChangePassword = function () {
                     new_password: {
                         validators: {
                             notEmpty: {
-                                message: 'New Password is required'
+                                message: 'Password is required'
+                            },
+                            stringLength: {
+                                min: 8,
+                                message: 'Password must be at least 8 characters'
                             },
                         }
                     },
@@ -28,6 +32,12 @@ var ChangePassword = function () {
                         validators: {
                             notEmpty: {
                                 message: 'Confirm Password is required'
+                            },
+                            identical: {
+                                compare: function() {
+                                    return $('input[name="new_password"]').val()
+                                },
+                                message: 'The password and confirm password are not the same'
                             }
                         }
                     },
@@ -39,6 +49,12 @@ var ChangePassword = function () {
                 }
             }
         );
+
+
+        KTUtil.getById("change-password-form").querySelector('[name="new_password"]').addEventListener('input', function() {
+            validation.revalidateField('confirm_password');
+        });
+
 
         $('input').keypress(function (e) {
           if (e.which == 13) {
@@ -54,60 +70,58 @@ var ChangePassword = function () {
             validation.validate().then(function(status) {
 
                 if (status == 'Valid') {
-                    var form = $('#change-password-form') 
-                    if($('input[name="new_password"]').val() == $('input[name="confirm_password"]').val() ){
-                        $.ajax({
-                            url: form.attr('action'),
-                            method: form.attr('method'),
-                            data: form.serialize(),
-                            dataType: "json",
-                            beforeSend: function () {
-                                $.blockUI({ 
-                                    message: '<h1><img src="' + BASE_URL + 'dist/assets/media/img/loader.gif" /> Please wait ...</h1>', 
-                                    css: { 
-                                        border: '0px !emportant', 
-                                        textAlign:      'center', 
-                                    },
-                                    showOverlay: false,
-                                    centerX: true,
-                                    centerY: true, 
-                                });
-                            },
-                            complete: function () {
-                                KTApp.unblock('body');
-                            },
-                            success: function (data) {
-                                console.info(data);
-                                if(!data.response){
-                                    Swal.fire({
-                                        title: data.message,
-                                        icon: "error",
-                                        showCancelButton: true, 
-                                    })
-                                }else{
-                                    Swal.fire({
-                                        title: data.message,
-                                        icon: "success",
-                                        showCancelButton: true, 
-                                    }).then(function(result) {
-                                        form[0].reset()
-                                        $('input[name="current_password"]').focus()
-                                        KTUtil.scrollTop()
-                                    });
-                                }  
-                            },
-                            error: function (xhr, status, error) {
-                                console.info(xhr.responseText);
-                            }
-                        });
+                    var form = $('#change-password-form')  
+                    $.ajax({
+                        url: form.attr('action'),
+                        method: form.attr('method'),
+                        data: form.serialize(),
+                        dataType: "json",
+                        beforeSend: function () {
+                            $.blockUI({ 
+                                message: '<h1><img src="' + BASE_URL + 'dist/assets/media/img/loader.gif" /> Please wait ...</h1>', 
+                                css: { 
+                                    border: '0px !emportant', 
+                                    textAlign:      'center', 
+                                },
+                                showOverlay: false,
+                                centerX: true,
+                                centerY: true, 
+                            });
+                        },
+                        complete: function () {
+                            KTApp.unblock('body');
+                        },
+                        success: function (data) {
+                            // console.info(data);
+                            if(!data.response){
 
-                    }else{
-                        Swal.fire({
-                            title: 'Password and Confirm Password dont\' match',
-                            icon: "error",
-                            showCancelButton: true, 
-                        })
-                    }
+                                ERROR_ALERT_SOUND.play()
+                                Swal.fire({
+                                    title: data.message,
+                                    icon: "error",
+                                    showCancelButton: true, 
+                                })
+                            }else{
+                                SUCCESS_ALERT_SOUND.play()
+                                Swal.fire({
+                                    title: data.message,
+                                    icon: "success",
+                                    showCancelButton: true, 
+                                }).then(function(result) {
+                                    window.location.href = BASE_URL + 'user'
+
+                                    // form[0].reset()
+                                    // $('input[name="current_password"]').focus()
+                                    // KTUtil.scrollTop()
+                                });
+                            }  
+                        },
+                        error: function (xhr, status, error) {
+                            console.info(xhr.responseText);
+                        }
+                    });
+
+                     
                 } else {
                     KTUtil.scrollTop();
                 }
