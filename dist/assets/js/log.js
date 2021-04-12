9f4e7bf8eeacda7e14062f5a42399f8bc3ff1044
John Cris <36905188+johncris09@users.noreply.github.com>
2021-04-12 14:13:58 +0800

    update log script

--- dist/assets/js/log.js
+++ dist/assets/js/log.js
@@ -1,22 +1,5 @@
 "use strict";
 $.fn.dataTable.ext.search.push(
-    function( settings, data, dataIndex ) {
-        var min = parseInt( $('#min').val(), 10 );
-        var max = parseInt( $('#max').val(), 10 );
-        var age = parseFloat( data[8] ) || 0; // use data for the age column
- 
-        if ( ( isNaN( min ) && isNaN( max ) ) ||
-             ( isNaN( min ) && age <= max ) ||
-             ( min <= age   && isNaN( max ) ) ||
-             ( min <= age   && age <= max ) )
-        {
-            return true;
-        }
-        return false;
-    }
-);
-
-$.fn.dataTable.ext.search.push(
     function (settings, data, dataIndex) {
         var min       = $('input[name="date-range-start"]').datepicker('getDate');
         var max       = $('input[name="date-range-end"]').datepicker('getDate');
@@ -50,86 +33,11 @@ var Log = function () {
         // client side proccessing
         var table = $('#log-table').DataTable({ 
              "scrollY": 450,
-             "scrollX": true,
-			// responsive: true, 
+             "scrollX": true, 
 			 dom:"<'row'<'col-sm-4 col-xs-4'l><'col-sm-4 col-xs-4 text-center'B><'col-sm-4 col-xs-4'f>>" +
                     "<'row'<'col-sm-12 col-xs-12'tr>>" +
                 "<'row'<'col-sm-5 col-xs-5'i><'col-sm-7 col-xs-7'p>>",
-			// buttons: ["print", "excelHtml5", "pdfHtml5", "colvis"],
             buttons:   [
-                // {
-                //     extend: 'print',
-                //     className: "btn btn-info btn-sm",
-                //     text: '<i class="fas fa-print"></i>',
-                //     title: 'Covid-19 Vaccine Pre Registration',
-                //     repeatingHead: {
-                //         logo: 'https://www.google.co.in/logos/doodles/2018/world-cup-2018-day-22-5384495837478912-s.png',
-                //         logoPosition: 'right',
-                //         logoStyle: '',
-                //         title: '<h3>Sample Heading</h3>'
-                //     },
-                //     titleAttr: 'Print',
-                //     autoPrint: false,
-                //     exportOptions: {
-                //       columns: ':visible'
-                //     },
-                //     customize: function ( win ) {
-                //         $(win.document.body).find( 'table' ).find('td:last-child, th:last-child').remove();
-                //         $(win.document.body)
-                //             .css( 'font-size', '9pt' )
-
-                //         $(win.document.body).find( 'table' )
-                //             .addClass( 'compact' )
-                //             .css( 'font-size', 'inherit' );
-
-                //         var last = null;
-                //         var current = null;
-                //         var bod = []; 
-                //         var css = '@page { size: landscape;margin: 0; border: none; }',
-                //             head = win.document.head || win.document.getElementsByTagName('head')[0],
-                //             style = win.document.createElement('style');
-         
-                //         style.type = 'text/css';
-                //         style.media = 'print';
-         
-                //         if (style.styleSheet)
-                //         {
-                //           style.styleSheet.cssText = css;
-                //         }
-                //         else
-                //         {
-                //           style.appendChild(win.document.createTextNode(css));
-                //         }
-         
-                //         head.appendChild(style);
-                //     }
-                //     // customize: function(win)
-                //     // {
-                //     //     $(win.document.body).find( 'table' ).find('td:last-child, th:last-child').remove();
-         
-                //     //     var last = null;
-                //     //     var current = null;
-                //     //     var bod = [];
-         
-                //     //     var css = '@page { size: landscape; }',
-                //     //         head = win.document.head || win.document.getElementsByTagName('head')[0],
-                //     //         style = win.document.createElement('style');
-         
-                //     //     style.type = 'text/css';
-                //     //     style.media = 'print';
-         
-                //     //     if (style.styleSheet)
-                //     //     {
-                //     //       style.styleSheet.cssText = css;
-                //     //     }
-                //     //     else
-                //     //     {
-                //     //       style.appendChild(win.document.createTextNode(css));
-                //     //     }
-         
-                //     //     head.appendChild(style);
-                //     // }
-                // },
                 {
                     extend: 'excelHtml5',
                     className: "btn btn-info btn-sm",
@@ -159,23 +67,9 @@ var Log = function () {
                     text: 'Column Visibility',
                     titleAttr: 'Column Visibility',
                     columns: ':not(:first-child)'
-                },
-                // {
-                //   extend: 'colvis',
-                //   className: "btn-sm",
-                //   text: 'Colonne'
-                // }, 
-            ], 
-            // scrollY: '50vh',
-            // scrollX: true,
-            // scrollCollapse: true,
-            // buttons: [
-            //     'print',
-            //     'copyHtml5',
-            //     'excelHtml5',
-            //     'csvHtml5',
-            //     'pdfHtml5',
-            // ],
+                }, 
+            ],
+            deferRender: true,
             ajax: {
                 url: BASE_URL + 'log/get_log',
                 type: 'POST',
@@ -238,36 +132,21 @@ var Log = function () {
                         <span></span>
                     </label>`;
 			},
-			// initComplete: function() {
-   //              var counter = 0;
-   //              this.api().columns().every(function() {
-   //                  var column = this;
-   //                  // console.info([counter, column.header().textContent] )
-   //                  switch ( column.header().textContent ) {
-   //                      case 'Purok':
-   //                          column.data().unique().sort().each(function(d, j) {
-   //                              $('.datatable-input[data-col-index="'+counter+'"]').append('<option value="' + d + '">' + d + '</option>');
-   //                          });
-   //                          break;
-   //                      case 'Street':
-   //                          column.data().unique().sort().each(function(d, j) {
-   //                              $('.datatable-input[data-col-index="'+counter+'"]').append('<option value="' + d + '">' + d + '</option>');
-   //                          });
-   //                          break;
-   //                      case 'Barangay':
-   //                          column.data().unique().sort().each(function(d, j) {
-   //                              $('.datatable-input[data-col-index="'+counter+'"]').append('<option value="' + d + '">' + d + '</option>');
-   //                          });
-   //                          break;
-   //                      case 'Occupation':
-   //                          column.data().unique().sort().each(function(d, j) {
-   //                              $('.datatable-input[data-col-index="'+counter+'"]').append('<option value="' + d + '">' + d + '</option>');
-   //                          });
-   //                          break;
-   //                  }
-   //                  counter++
-   //              });
-   //          },
+			initComplete: function() {
+                var counter = 0;
+                this.api().columns().every(function() {
+                    var column = this;
+                    console.info( column.header().textContent)
+                    switch ( column.header().textContent ) {
+                        case 'User':
+                            column.data().unique().sort().each(function(d, j) {
+                                $('.datatable-input[data-col-index="'+counter+'"]').append('<option value="' + d + '">' + d + '</option>');
+                            });
+                            break;
+                    }
+                    counter++
+                });
+            },
             columnDefs: [
 				{
 					targets: 0,
@@ -280,96 +159,6 @@ var Log = function () {
                         </label>`;
 					},
 				},
-                // {
-                //     targets  : 1,
-                //     title    : 'Actions',
-                //     orderable: false,
-                //     render   : function(data, type, row, meta) { 
-                //         var action  = '';
-
-                //         if(ROLE_TYPE.toLowerCase() == 'super admin'){
-                //             action =  '\
-                //                 <div class = "dropdown dropdown-inline">\
-                //                     <a href = "javascript:;" class = "btn btn-sm btn-clean btn-icon" data-toggle = "dropdown">\
-                //                         <i class = "fa fa-cog text-primary"></i>\
-                //                     </a>\
-                //                     <div class = "dropdown-menu dropdown-menu-sm dropdown-menu-right">\
-                //                         <ul class = "nav nav-hoverable flex-column">\
-                //                             <li class = "nav-item">\
-                //                                 <a href="javascript:;" data-id="'+row.id+'" class="nav-link validate-log">\
-                //                                     <i class = "nav-icon fas fa-stamp text-info"></i>\
-                //                                     <span class = "nav-text text-info" title="Validte">Validate</span>\
-                //                                 </a>\
-                //                             </li>\
-                //                             <li class = "nav-item">\
-                //                                 <a href="'+BASE_URL+'log/view/'+row.id+'" class="nav-link">\
-                //                                     <i class = "nav-icon fas far fa-eye text-primary"></i>\
-                //                                     <span class = "nav-text text-primary" title="View Details">View Details</span>\
-                //                                 </a>\
-                //                             </li>\
-                //                             <li class = "nav-item">\
-                //                                 <a href="'+BASE_URL+'log/edit/'+row.id+'" class = "nav-link">\
-                //                                     <i class = "nav-icon fas fa-pencil-alt text-warning"></i>\
-                //                                     <span class = "nav-text text-warning" title="Edit Details">Edit Details</span>\
-                //                                 </a>\
-                //                             </li>\
-                //                             <li class = "nav-item">\
-                //                                 <a href="javascript:;" data-id="'+row.id+'" class="nav-link delete-log">\
-                //                                     <i class = "nav-icon la la-trash text-danger"></i>\
-                //                                     <span class = "nav-text text-danger" title="Delete">Delete</span>\
-                //                                 </a>\
-                //                             </li>\
-                //                         </ul>\
-                //                     </div>\
-                //                 </div>\
-                //             ';
-                //         }else if(ROLE_TYPE.toLowerCase() == 'sub admin'){
-                //             action =  '\
-                //                 <div class = "dropdown dropdown-inline">\
-                //                     <a href = "javascript:;" class = "btn btn-sm btn-clean btn-icon" data-toggle = "dropdown">\
-                //                         <i class = "fa fa-cog text-primary"></i>\
-                //                     </a>\
-                //                     <div class = "dropdown-menu dropdown-menu-sm dropdown-menu-right">\
-                //                         <ul class = "nav nav-hoverable flex-column">\
-                //                             <li class = "nav-item">\
-                //                                 <a href="'+BASE_URL+'log/view/'+row.id+'" class = "nav-link delete-category">\
-                //                                     <i class = "nav-icon fas far fa-eye text-primary"></i>\
-                //                                     <span class = "nav-text text-primary">View</span>\
-                //                                 </a>\
-                //                             </li>\
-                //                             <li class = "nav-item">\
-                //                                 <a href="'+BASE_URL+'log/edit/'+row.id+'" class = "nav-link delete-category">\
-                //                                     <i class = "nav-icon fas fa-pencil-alt text-warning"></i>\
-                //                                     <span class = "nav-text text-warning">Edit Details</span>\
-                //                                 </a>\
-                //                             </li>\
-                //                         </ul>\
-                //                     </div>\
-                //                 </div>\
-                //             ';
-                //         }else if(ROLE_TYPE.toLowerCase() == 'user'){
-                //             action =  '\
-                //                 <div class = "dropdown dropdown-inline">\
-                //                     <a href = "javascript:;" class = "btn btn-sm btn-clean btn-icon" data-toggle = "dropdown">\
-                //                         <i class = "fa fa-cog text-primary"></i>\
-                //                     </a>\
-                //                     <div class = "dropdown-menu dropdown-menu-sm dropdown-menu-right">\
-                //                         <ul class = "nav nav-hoverable flex-column">\
-                //                             <li class = "nav-item">\
-                //                                 <a href="'+BASE_URL+'log/view/'+row.id+'" class = "nav-link delete-category">\
-                //                                     <i class = "nav-icon fas far fa-eye text-primary"></i>\
-                //                                     <span class = "nav-text text-primary">View</span>\
-                //                                 </a>\
-                //                             </li>\
-                //                         </ul>\
-                //                     </div>\
-                //                 </div>\
-                //             ';
-                //         }
-                        
-                //         return action;
-                //     },
-                // },
             ],
         });
  
