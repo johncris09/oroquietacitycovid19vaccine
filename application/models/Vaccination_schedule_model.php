6966051e08fa6f95f5a141ba55dac54a76367a85
John Cris <36905188+johncris09@users.noreply.github.com>
2021-04-12 14:36:54 +0800

    vaccination schedule model

---
+++ application/models/Vaccination_schedule_model.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Vaccination_schedule_model extends CI_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function insert($data)
+    {
+        $this->db->db_debug = false;
+        $insert = $this->db->insert('vaccination_schedule', $data);
+        if(!$insert && $this->db->error()['code'] == 1062){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+
+}
