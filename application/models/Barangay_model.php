5bc3fd5f8ea29eefdc2d4279c3097b4b2766af8a
John Cris <36905188+johncris09@users.noreply.github.com>
2021-04-12 13:55:32 +0800

    get all barangay which is not in assign barangay

--- application/models/Barangay_model.php
+++ application/models/Barangay_model.php
@@ -8,4 +8,13 @@ class Barangay_model extends CI_Model
     {
         parent::__construct();
     }
+
+    public function barangay()
+    {
+        return $this->db
+            ->where('barangay.barangay_id not in (SELECT assign_barangay.barangay FROM `assign_barangay`
+where assign_barangay.barangay)')
+            ->get('barangay')
+            ->result_array();
+    }
 }
