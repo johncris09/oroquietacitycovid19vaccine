868c46ca499c33d643148bb120704d8accae033f
John Cris <36905188+johncris09@users.noreply.github.com>
2021-04-12 13:54:25 +0800

    init

---
+++ application/models/Barangay_model.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Barangay_model extends CI_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
